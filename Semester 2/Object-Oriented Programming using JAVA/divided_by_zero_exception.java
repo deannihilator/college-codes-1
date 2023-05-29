40653ae34815afbe52f49cfc26a73c921ad98576
Animesh Sharma <sharmaanimesh2004@gmail.com>
2023-05-29 22:57:33 +0530

    Update divided_by_zero_exception.java

--- Semester 2/Object-Oriented Programming using JAVA/divided_by_zero_exception.java
+++ Semester 2/Object-Oriented Programming using JAVA/divided_by_zero_exception.java
@@ -1,12 +1,16 @@
 // Write a program to demonstrate a division by zero exception
-public class divided_by_zero_exception {
+public class divided_by_zero_exception
+{
     static int a, b;
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         a = 15;
         b = 0;
-        try {
+        try 
+        {
             System.out.println("The result is: " + (a / b));
-        } catch (ArithmeticException ae) {
+        }
+        catch (ArithmeticException ae) {
             System.out.println("Second number cannot be zero");
         }
     }
